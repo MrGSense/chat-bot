9c41d1ce914b049783569aabd973c6de956bf7c7
Kyle <kyle.fetterhoff@gmail.com>
2020-02-12 12:50:16 -0500

    Add ability to text query dialogflow

--- routes/diaglogFlowRoutes.js
+++ routes/diaglogFlowRoutes.js
@@ -1,35 +1,13 @@
-const dialogflow = require("dialogflow");
-const config = require("../config/keys");
-
-const sessionClient = new dialogflow.SessionsClient();
-
-const sessionPath = sessionClient.sessionPath(
-  config.googleProjectID,
-  config.dialogFlowSessionID
-);
+const chatbot = require("../chatbot/chatbot");
 
 module.exports = app => {
-  app.post("/api/df_text_query", (req, res) => {
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          text: request.body.text,
-          languageCode: config.dialogFlowSessionLanguageCode
-        }
-      }
-    };
+  app.post("/api/df_text_query", async (req, res) => {
+    let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+
+    res.send(responses[0].queryResult);
   });
 
-  app.post("/api/df_event_query", (req, res) => {
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          text: request.body.text,
-          languageCode: config.dialogFlowSessionLanguageCode
-        }
-      }
-    };
+  app.post("/api/df_event_query", async (req, res) => {
+    res.send("Hello");
   });
 };
