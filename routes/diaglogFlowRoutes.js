bbff75154a71e3b236167a4ee4482b4c648f53ec
Kyle <kyle.fetterhoff@gmail.com>
2020-02-12 12:26:44 -0500

    Connect server to dialogflow

--- routes/diaglogFlowRoutes.js
+++ routes/diaglogFlowRoutes.js
@@ -1,9 +1,35 @@
+const dialogflow = require("dialogflow");
+const config = require("../config/keys");
+
+const sessionClient = new dialogflow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(
+  config.googleProjectID,
+  config.dialogFlowSessionID
+);
+
 modules.exports = app => {
   app.post("/api/df_text_query", (res, res) => {
-    res.send();
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: request.body.text,
+          languageCode: config.dialogFlowSessionLanguageCode
+        }
+      }
+    };
   });
 
   app.post("/api/df_event_query", (res, res) => {
-    res.send();
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: request.body.text,
+          languageCode: config.dialogFlowSessionLanguageCode
+        }
+      }
+    };
   });
 };
