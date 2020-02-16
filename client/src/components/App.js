64e7cc29257f9ebd33bb71b49743d305c3b82401
Kyle <kyle.fetterhoff@gmail.com>
2020-02-15 20:29:59 -0500

    Add Routing and Header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import About from "./pages/About";
+import Shop from "./shop/Shop";
+import Header from "./Header";
+
 import "./styles/app.css";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Chat Bot</h1>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/shop' component={Shop} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
