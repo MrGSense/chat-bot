63e8047ecc98683c7c42b37d4b09f0582fa71fe4
Kyle <kyle.fetterhoff@gmail.com>
2020-02-15 20:32:02 -0500

    Add initial chatbot component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import Landing from "./pages/Landing";
 import About from "./pages/About";
 import Shop from "./shop/Shop";
 import Header from "./Header";
+import Chatbot from "./chatbot/Chatbot";
 
 import "./styles/app.css";
 
@@ -18,6 +19,7 @@ class App extends Component {
             <Route exact path='/' component={Landing} />
             <Route exact path='/about' component={About} />
             <Route exact path='/shop' component={Shop} />
+            <Chatbot />
           </div>
         </BrowserRouter>
       </div>
