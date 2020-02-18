7a2e2efcff0d52848331e77fb239eefeadfe907a
Kyle <kyle.fetterhoff@gmail.com>
2020-02-17 19:51:22 -0500

    Add auto scrolling and bot interaction

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -4,12 +4,14 @@ import axios from "axios";
 import Message from "./Message";
 
 class Chatbot extends Component {
+  messagesEnd;
   constructor(props) {
     super(props);
 
     this.state = {
       messages: []
     };
+    this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
   }
 
   async df_text_query(text) {
@@ -53,6 +55,10 @@ class Chatbot extends Component {
     this.df_event_query("Welcome");
   }
 
+  componentDidUpdate() {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
+
   renderMessages(stateMessages) {
     if (stateMessages) {
       return stateMessages.map((message, i) => {
@@ -69,6 +75,13 @@ class Chatbot extends Component {
     }
   }
 
+  _handleInputKeyPress(e) {
+    if (e.key === "Enter") {
+      this.df_text_query(e.target.value);
+      e.target.value = "";
+    }
+  }
+
   render() {
     return (
       <div style={{ height: 400, width: 400, float: "right" }}>
@@ -78,7 +91,13 @@ class Chatbot extends Component {
         >
           <h2>Chatbot</h2>
           {this.renderMessages(this.state.messages)}
-          <input type='text' />
+          <div
+            ref={el => {
+              this.messagesEnd = el;
+            }}
+            style={{ float: "left", clear: "both" }}
+          ></div>
+          <input type='text' onKeyPress={this._handleInputKeyPress} />
         </div>
       </div>
     );
