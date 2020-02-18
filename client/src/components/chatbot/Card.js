1a8d1df39182b3429aa57b5f28fcac19ee94bed1
Kyle <kyle.fetterhoff@gmail.com>
2020-02-18 11:49:36 -0500

    Add Card component for chatbot

---
+++ client/src/components/chatbot/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Card = props => {
+  return (
+    <div style={{ float: "left", paddingRight: 30, width: 270 }}>
+      <div className='card'>
+        <div className='card-image' style={{ width: 240 }}>
+          <img
+            alt={props.payload.fields.header.stringValue}
+            src={props.payload.fields.image.stringValue}
+          />
+          >
+          <span className='card-title'>
+            {props.payload.fields.header.stringValue}
+          </span>
+        </div>
+        <div className='card-content'>
+          {props.payload.fields.description.stringValue}
+          <p>
+            <a>{props.payload.fields.price.stringValue}</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
