3161d942197db67a78314e2c640b0af9b2ea21da
Kyle <kyle.fetterhoff@gmail.com>
2020-02-17 20:04:58 -0500

    Add userID to cookies for session

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Cookies from "universal-cookie";
+import { v4 as uuid } from "uuid";
 
 import Message from "./Message";
+
+const cookies = new Cookies();
 
 class Chatbot extends Component {
   messagesEnd;
@@ -11,22 +15,30 @@ class Chatbot extends Component {
     this.state = {
       messages: []
     };
+
     this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+
+    if (cookies.get("userID" === undefined)) {
+      cookies.set("userID", uuid(), { path: "/" });
+    }
   }
 
-  async df_text_query(text) {
+  async df_text_query(queryText) {
     let says = {
       speaks: "me",
       msg: {
         text: {
-          text: text
+          text: queryText
         }
       }
     };
 
     this.setState({ messages: [...this.state.messages, says] });
 
-    const res = await axios.post("/api/df_text_query", { text });
+    const res = await axios.post("/api/df_text_query", {
+      text: queryText,
+      userID: cookies.get("userID")
+    });
 
     for (let msg of res.data.fulfillmentMessages) {
       says = {
@@ -38,8 +50,11 @@ class Chatbot extends Component {
     }
   }
 
-  async df_event_query(event) {
-    const res = await axios.post("/api/df_event_query", { event });
+  async df_event_query(eventName) {
+    const res = await axios.post("/api/df_event_query", {
+      event: eventName,
+      userID: cookies.get("userID")
+    });
 
     for (let msg of res.data.fulfillmentMessages) {
       let says = {
