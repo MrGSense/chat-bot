b48668e710d56025b18a94ad3914f1536e520165
Kyle <kyle.fetterhoff@gmail.com>
2020-02-23 15:35:40 -0500

    Add route for serving front-end in production

--- server.js
+++ server.js
@@ -19,6 +19,15 @@ require("./models/Coupons");
 require("./routes/diaglogFlowRoutes")(app);
 require("./routes/fulfillmentRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(client / dist));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port);
